03edea7326d3d986814e0b6d705657e5a3205b60
CataBasso <cbasso@fi.uba.ar>
2026-09-18 20:16:15 +0000

    Implementacion necesaria para el escenario 2: "Multiples clientes, una sola replica de cada elemento" - Se agrega un ID a cada cliente - El "message_handler" lo incorpora en los mensajes que entran en la pipeline - "sum" hace la suma por cliente y luego manda los resultados y el EOF de cada uno - "aggregation" hace un top por cliente y cuando recibe el EOF genera un top por cliente

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -3,16 +3,26 @@ from common import message_protocol
 
 class MessageHandler:
 
+    next_client_id = 0
+
     def __init__(self):
-        pass
+        self.client_id = MessageHandler.next_client_id
+        MessageHandler.next_client_id += 1
     
     def serialize_data_message(self, message):
         [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
+        return message_protocol.internal.serialize([self.client_id, fruit, amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+        return message_protocol.internal.serialize([self.client_id])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
-        return fields
+
+        client_id = fields[0]
+        fruit_top = fields[1]
+
+        if client_id != self.client_id:
+            return []
+
+        return fruit_top
